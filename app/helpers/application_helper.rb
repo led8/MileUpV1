8592a0f242a860102f5b8c03f05be11b6a448d87
Arthur <a.dhuy@orange.fr>
2019-07-18 13:57:01 +0200

    generate devise Model + View

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def svg(name)
     file_path = "#{Rails.root}/app/assets/images/svg/#{name}.svg"
     if File.exists?(file_path)
-      cache { File.read(file_path).html_safe }
+      File.read(file_path).html_safe
     else
       '(not found)'
     end
