f990d9991ca60e3155dbf033e11b7b7556ac0cc0
Arthur <a.dhuy@orange.fr>
2019-07-18 12:15:49 +0200

    add svg helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/svg/#{name}.svg"
+    if File.exists?(file_path)
+      cache { File.read(file_path).html_safe }
+    else
+      '(not found)'
+    end
+  end
 end
