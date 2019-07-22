df09de84ea3e2ed1142b62c146b38959cb7935c3
Arthur <a.dhuy@orange.fr>
2019-07-22 16:44:26 +0200

    add some navbar animation

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,12 +1,16 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-mileup');
+  const navbarLink = document.getElementById('becoming-partner');
+
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-mileup-border');
-        console.log("The window is scrolling !!")
-      } else {
-        navbar.classList.remove('navbar-mileup-border');
+      if(window.scrollY >= 50) {
+        navbar.classList.add('navbar-mileup-purple');
+        navbarLink.classList.add('nav-link-white');
+      }
+      else {
+        navbar.classList.remove('navbar-mileup-purple');
+        navbarLink.classList.remove('nav-link-white');
       }
     });
   }
