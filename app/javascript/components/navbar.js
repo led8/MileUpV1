3e2bf941607c1606e927d6368eb5f39d5b29f8fe
Arthur <a.dhuy@orange.fr>
2019-07-22 22:47:02 +0200

    adjust navbar animation

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,16 +1,23 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-mileup');
   const navbarLink = document.getElementById('becoming-partner');
+  var loginButton = $('.navbar-login-button');
 
   if (navbar) {
     window.addEventListener('scroll', () => {
+
       if(window.scrollY >= 50) {
-        navbar.classList.add('navbar-mileup-purple');
-        navbarLink.classList.add('nav-link-white');
+        navbar.classList.add('purple');
+        navbarLink.classList.add('white');
+      } else {
+        navbar.classList.remove('purple');
+        navbarLink.classList.remove('white');
       }
-      else {
-        navbar.classList.remove('navbar-mileup-purple');
-        navbarLink.classList.remove('nav-link-white');
+
+      if(window.scrollY >= 670) {
+        loginButton.fadeIn(1000);
+      } else {
+        loginButton.fadeOut(1600);
       }
     });
   }
