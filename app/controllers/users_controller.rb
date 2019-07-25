907170952be0c7b9042d5f810b2616b2e184cfe4
Arthur <a.dhuy@orange.fr>
2019-07-25 10:14:30 +0200

    create specific redirection when user sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def show
+    render :show
+  end
+
+  def test
+    render :test
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks',
+                                    registrations: 'registrations' }
   root to: 'pages#home'
+
+  ## TESTS
+  get 'test', to: 'users#test'
+
+  ## USERS ROUTES
+  resources :users, only: [:show]
 
   ## FACEBOOK AUTHENTIFICATION
   authenticated :user do
@@ -12,7 +19,7 @@ Rails.application.routes.draw do
   resources :authentications, only: [:destroy]
 
 
-  ## CSV ROOTS
+  ## CSV ROUTES
   get 'create_contact_models_from_csv', to: 'pages#create_contact_models_from_csv'
   get 'create_city_equipments_models_from_csv', to: 'pages#create_city_equipments_models_from_csv'
   get 'create_info_equipments_models_from_csv', to: 'pages#create_info_equipments_models_from_csv'
