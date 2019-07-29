0eb7c211a70a8062f070ab390d12ff56b733e3f9
Arthur <a.dhuy@orange.fr>
2019-07-29 17:55:11 +0200

    Carrierwave set

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
 
-  has_one :user_profil
+  has_one :user_profil, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
 
