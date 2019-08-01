529cf90f0c1611df45810c8f228573d5553d1db3
Arthur <a.dhuy@orange.fr>
2019-08-01 16:28:36 +0200

    add user track location on map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,6 +1,8 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+// HOME MAP //
+
 const initMapboxHome = () => {
   const mapElement = document.getElementById('map');
 
@@ -9,8 +11,14 @@ const initMapboxHome = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     const map = new mapboxgl.Map({
       container: 'map',
-      style: "mapbox://styles/ledhuy/cjygzo2kc008n1cl539vyk7lg"
+      style: 'mapbox://styles/ledhuy/cjygzo2kc008n1cl539vyk7lg',
+      center: [-96, 37.8],
+      zoom: 3
     });
+
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: { enableHighAccuracy: true }, trackUserLocation: true
+    }));
     addMarkers(map, markers);
     fitMapToMarkers(map, markers);
   }
@@ -33,6 +41,37 @@ const fitMapToMarkers = (map, markers) => {
 };
 
 initMapboxHome();
+
+// EQUIPMENT PAGE MAP //
+
+const initMapboxEquipment = () => {
+  const mapElement = document.getElementById('equipment-map');
+
+  if (mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const marker = JSON.parse(mapElement.dataset.marker);
+    const map = new mapboxgl.Map({
+      container: 'equipment-map',
+      style: "mapbox://styles/ledhuy/cjygzo2kc008n1cl539vyk7lg"
+    });
+    addMarkerOnEquipment(map, marker);
+    fitMapToMarkerOnEquipment(map, marker);
+  }
+};
+
+const addMarkerOnEquipment = (map, marker) => {
+  new mapboxgl.Marker()
+    .setLngLat([ marker.lat, marker.lng ])
+    .addTo(map);
+}
+
+const fitMapToMarkerOnEquipment = (map, marker) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  bounds.extend([ marker.lat, marker.lng ]);
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+initMapboxEquipment();
 
 // AJAX //
 
