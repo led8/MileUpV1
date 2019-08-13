8a3751dd335d45561eb20749e5555187197b15e2
Arthur <a.dhuy@orange.fr>
2019-08-13 17:33:43 +0200

    prep work about structured data

--- app/models/info_equipment.rb
+++ app/models/info_equipment.rb
@@ -1,7 +1,9 @@
 class InfoEquipment < ApplicationRecord
+  # SEO for Friendly's URL
   extend FriendlyId
   friendly_id :name, use: :slugged
 
+  # PgSearch
   include PgSearch
   pg_search_scope :search_by_activity, against: [ :activity ], using: { tsearch: { prefix: true } }
 end
